fb857641eaa1351f337291556a48f598a7a0c721
rmpcoding <rmpcoding@gmail.com>
2026-01-13 12:16:50 -0800

    fix: connection URI to db

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,15 +1,19 @@
 import { connect, set } from "mongoose";
 
-const MONGO_DB_URI = process.env.MONGO_DB_URI || "";
+const MONGO_DB_URI =
+  process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/options-tracker-db";
+const ENV = process.env.NODE_ENV;
+
+if (!MONGO_DB_URI && ENV !== "production") {
+  throw new Error(`MONGODB URI is not set`);
+}
 
 export const connectToDB = async () => {
   try {
     set("strictQuery", false);
     const db = await connect(MONGO_DB_URI);
-    // Emit log when the connection is successful
-    console.log("MongoDB connected to", db.connection.name);
+    console.log(`🛢  MongoDB connected to ${db.connection.name} 🛢`);
   } catch (error) {
-    // Emit log when there's an error
-    console.error(error);
+    throw new Error(`DB connection error: ${error}`);
   }
 };
